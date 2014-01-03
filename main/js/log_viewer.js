d0886566871d8fb61694dfef71009b128d5fae4b
uthornbl <ulf@pax4.nl>
2014-01-03 09:15:22 +0100

    Introduced date/time slider.

--- main/js/log_viewer.js
+++ main/js/log_viewer.js
@@ -63,6 +63,21 @@ function pagePrev() {
   }
 }
 
+// Go to specific page
+function pageGoTo(pg) {
+  var ptr = ( pg - 1 ) * linesPerPage;
+  pageNo = pg;
+  displayLines(ptr);
+}
+
+// Go to the last page
+function pageEOF() {
+  var lastLine = ( filtered ) ? filterMap.length - 1 : data.log.length - 1;
+  var ptr = ( lastLine >= linesPerPage ) ? lastLine - linesPerPage + 1 : 0;
+  pageNo = Math.floor( ptr / linesPerPage ) + 1;
+  displayLines(ptr);
+}
+
 function clearFilter() {
   filtered = false;
   $("#filter_msg").val("");
@@ -114,6 +129,11 @@ function filter(expr) {
 
 function displayLines(start) {
   curPtr = start;
+  var lastLine = ( filtered ) ? filterMap.length - 1 : data.log.length - 1;
+  var lastPage = Math.floor( ( lastLine + 1 ) / linesPerPage );
+  var lp2 = Math.ceil( ( lastLine + 1 ) / linesPerPage );
+  if (lp2 > lastPage )
+    lastPage++;
 
   var log_filter_elm = "<select id=\"filter_loglevel\" onChange=\"javascript:fixFilter();\">"
     + "<option " + "value=\"\">-- ANY --</option>";
@@ -125,9 +145,13 @@ function displayLines(start) {
 
   var msg_filter_elm = "<input value=\"" + filter_msg + "\" id=\"filter_msg\" maxlength='30' size='10' onChange=\"javascript:fixFilter();\">"
 
-  var html = "<tbody>"
+  // var html = "<tbody class=\"hive\">"
+  var html = ""
     + "<tr class=\"big\">"
-    + "<th><button id=\"prevPage\">&lt;&lt;</button>" + pageNo + "<button id=\"nextPage\">&gt;&gt;</button></th>"
+    + "<th class=\"tsheader\"><button id=\"prevPage\">&lt;</button>" + pageNo + "<button id=\"nextPage\">&gt;</button>"
+    + "&nbsp;<button id=\"eofPage\">&gt;&gt;</button>"
+    + "<br/><div id=\"tslide\" style=\"margin-top: 5px;\"></div>"
+    + "</th>"
     + "<th>Thread</th>"
     + "<th>LogLevel</th>"
     + "<th>Class</th>"
@@ -146,17 +170,17 @@ function displayLines(start) {
 
   for (var offset=0; offset<linesPerPage; offset++) {
     var lineNo = start + offset;
+    if (lineNo >= data.log.length)
+      break; // We've reached EOF
     if (filtered) {
-      if (lineNo > filterMap.length - 1)
-        break;
+      if (lineNo >= filterMap.length)
+        break; // We've reached EOF
       lineNo = filterMap[lineNo];
     }
-    var dt = new Date( Date(data.log[lineNo].ts));
-    var mth = dt.getMonth()+1;
-    var stamp = dt.getFullYear() + "-" + mth + "-" + dt.getDate() + " " +
-      dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds() + "." + dt.getMilliseconds();
-    stamp = sprintf("%04d-%02d-%02d %02d:%02d:%02d", dt.getFullYear(),
-      mth, dt.getDate(), dt.getHours(), dt.getMinutes(), dt.getSeconds());
+
+    // var dt = new Date( Date(data.log[lineNo].ts ));
+    var dt = new Date( data.log[lineNo].ts / 1 );
+    var ltime = dt.toISOString().match(/(\d{4}\-\d{2}\-\d{2})T(\d{2}:\d{2}:\d{2})/);
     // Highlight GUIDs
     var msg = data.log[lineNo].msg.replace(/(guid[:= ]*)([0-9a-f]{20,})/ig, "$1\<span class=\"highlight-guid\"\>$2\<\/span\>");
     // Highlight search hits
@@ -165,20 +189,28 @@ function displayLines(start) {
 
 
     html += "<tr class=\"log-" + data.log[lineNo].ll.toLowerCase() + "\">"
-      + "<td>" + stamp + "</td>"
+      + "<td>" + ltime[1] + " " + ltime[2] + "</td>"
       + "<td>" + data.log[lineNo].th + "</td>"
       + "<td>" + data.log[lineNo].ll + "</td>"
       + "<td>" + data.log[lineNo].cl + "</td>"
       + "<td>" + msg + "</td>"
       + "</tr>";
   }
-  html += "</tbody>";
+  // html += "</tbody>";
   dataTable.html(html);
+  $("#tslide").slider( { value: pageNo, min: 1, max: lastPage, orientation: "horizontal",
+      change: function(event, ui) {
+        pageGoTo(ui.value);
+      }
+    });
   $("#nextPage").click(
     function() { pageNext(); }
   );
   $("#prevPage").click(
     function() { pagePrev(); }
+  );
+  $("#eofPage").click(
+    function() { pageEOF(); }
   );
   $("#clearFilter").click(
     function() { clearFilter(); }
